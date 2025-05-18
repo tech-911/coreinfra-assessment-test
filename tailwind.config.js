0de7b788e1c6e3fcb21846f73885480392462237
tech-911 <babatundeolatunji74@gmail.com>
2025-05-18 22:04:55 +0100

    ...updating 3

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    // Add other directories or files where Tailwind classes are used
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
